113f01025f19465b0e3db27d4087af6206d9bd06
Italo <104892321+Italo115@users.noreply.github.com>
2024-07-13 12:22:01 +0200

    Testing

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -29,7 +29,7 @@ public class Main {
         }
             else if (HttPRequest[1].equals("user-agent"))
         {
-            System.out.println(HttPRequest[1]);
+            System.out.println("PrintStatement hereeee : "+ HttPRequest[1]);
             output.write(String.format("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %s\r\n\r\n%s\r\n", reader.readLine().split("\\s+")[1].length(), reader.readLine().split("\\s+")[1]).getBytes());
         }
         else {
