a6937fb9c8e314a1d09da9dcd9c1c873729d22ec
Italo <104892321+Italo115@users.noreply.github.com>
2024-07-13 11:55:42 +0200

    pass 1st State

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,32 +6,33 @@ public class Main {
   public static void main(String[] args) {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     System.out.println("Logs from your program will appear here!");
-
     ServerSocket serverSocket = null;
     Socket clientSocket = null;
 
-
-     try {
+    try {
         serverSocket = new ServerSocket(4221);
         serverSocket.setReuseAddress(true);
         clientSocket = serverSocket.accept(); // Wait for connection from client.
         InputStream input = clientSocket.getInputStream();
-         BufferedReader reader = new BufferedReader(new InputStreamReader(input));
-         String line = reader.readLine();
-         System.out.println(line);
-         String[] HttPRequest = line.split(" ", 0);
-         OutputStream output = clientSocket.getOutputStream();
-         if (HttPRequest[1].equals("/"))
-             output.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
-         else
-             output.write("HTTP/1.1 404 Not Found\r\n\r\n".getBytes());
+        BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+        String line = reader.readLine();
+        System.out.println(line);
+        String[] HttPRequest = line.split(" ", 0);
+        OutputStream output = clientSocket.getOutputStream();
+
+        if (HttPRequest[1].equals("/"))
+            output.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+
+        else if (HttPRequest[1].startsWith("/echo/"))
+            clientSocket.getOutputStream().write(String.format("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s",
+            HttPRequest[1].substring(6).length(), HttPRequest[1].substring(6)).getBytes());
+        else
+            output.write("HTTP/1.1 404 Not Found\r\n\r\n".getBytes());
 
         System.out.println("accepted new connection");
-
-
-     } catch (IOException e) {
-       System.out.println("IOException: " + e.getMessage());
-     }
+    } catch (IOException e) {
+        System.out.println("IOException: " + e.getMessage());
+    }
   }
 }
 
