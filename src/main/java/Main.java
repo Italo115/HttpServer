266e18841ca3de376b8d5fbdc27a02a4e9d2254d
Italo <104892321+Italo115@users.noreply.github.com>
2024-07-13 14:11:36 +0200

    Testing

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -17,22 +17,18 @@ public class Main {
         InputStream input = clientSocket.getInputStream();
         BufferedReader reader = new BufferedReader(new InputStreamReader(input));
         String line = reader.readLine();
-        System.out.println(line);
-        String[] HttPRequest = line.split(" ", 0);
+        String[] HttpRequest = line.split(" ", 0);
         OutputStream output = clientSocket.getOutputStream();
 
-        if (HttPRequest[1].equals("/")) {
+        if (HttpRequest[1].equals("/")) {
             output.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
         }
-        else if (HttPRequest[1].startsWith("/echo/")) {
+        else if (HttpRequest[1].startsWith("/echo/")) {
             clientSocket.getOutputStream().write(String.format("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s",
-                    HttPRequest[1].substring(6).length(), HttPRequest[1].substring(6)).getBytes());
+                    HttpRequest[1].substring(6).length(), HttpRequest[1].substring(6)).getBytes());
         }
-            else if (HttPRequest[1].equals("/user-agent"))
+            else if (HttpRequest[1].equals("/user-agent"))
         {
-            reader.readLine();
-            System.out.println("PrintStatement hereeee : "+ HttPRequest[1]);
-
             output.write(String.format("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %s\r\n\r\n%s\r\n", reader.readLine().split("\\s+")[1].length(), reader.readLine().split("\\s+")[1]).getBytes());
         }
         else {
