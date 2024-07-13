30c6a6faca9f0bee90d02c1345d1b972213ab133
Italo <104892321+Italo115@users.noreply.github.com>
2024-07-13 11:44:32 +0200

    pass 1st State

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,4 @@
-import java.io.IOException;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -13,14 +13,21 @@ public class Main {
 
      try {
         serverSocket = new ServerSocket(4221);
-
-        // Since the tester restarts your program quite often, setting SO_REUSEADDR
-        // ensures that we don't run into 'Address already in use' errors
         serverSocket.setReuseAddress(true);
         clientSocket = serverSocket.accept(); // Wait for connection from client.
+        InputStream input = clientSocket.getInputStream();
+         BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+         String line = reader.readLine();
+         System.out.println(line);
+         String[] HttPRequest = line.split(" ", 0);
+         OutputStream output = clientSocket.getOutputStream();
+         if (HttPRequest[1].equals("/"))
+             output.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+         else
+             output.write("HTTP/1.1 404 Not Found\r\n\r\n".getBytes());
 
         System.out.println("accepted new connection");
-        clientSocket.getOutputStream().write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+
 
      } catch (IOException e) {
        System.out.println("IOException: " + e.getMessage());
