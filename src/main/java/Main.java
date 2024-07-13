0dcfa1bd9dae07a5664f56214a1bc0645ba41966
Italo <104892321+Italo115@users.noreply.github.com>
2024-07-13 11:33:52 +0200

    pass 1st State

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,17 +7,21 @@ public class Main {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     System.out.println("Logs from your program will appear here!");
 
+    ServerSocket serverSocket = null;
+    Socket clientSocket = null;
 
 
      try {
-       ServerSocket serverSocket = new ServerSocket(4221);
+        serverSocket = new ServerSocket(4221);
 
-       // Since the tester restarts your program quite often, setting SO_REUSEADDR
-       // ensures that we don't run into 'Address already in use' errors
-       serverSocket.setReuseAddress(true);
+        // Since the tester restarts your program quite often, setting SO_REUSEADDR
+        // ensures that we don't run into 'Address already in use' errors
+        serverSocket.setReuseAddress(true);
+        clientSocket = serverSocket.accept(); // Wait for connection from client.
 
-       serverSocket.accept(); // Wait for connection from client.
-       System.out.println("accepted new connection");
+        System.out.println("accepted new connection");
+        clientSocket.getOutputStream().write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+
      } catch (IOException e) {
        System.out.println("IOException: " + e.getMessage());
      }
